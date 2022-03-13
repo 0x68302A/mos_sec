f8edac87ca492a78701ae59244d850b90be87d7f
Aran Ailbhe <mosp08@protonmail.com>
2022-03-13 19:58:08 +0200

    Continuation on 3962781.

--- pkg/mos-tbb_patch.d/mos-tbb_patch.sh
+++ pkg/mos-tbb_patch.d/mos-tbb_patch.sh
@@ -2,3 +2,5 @@
 
 tar -xvf ./tor-browser-*
 cp ./user.js tor-browser_en-US/Browser/TorBrowser/Data/Browser/profile.default/
+echo "cd /opt/mos-tbb_patch.d/tor-browser_en-US/ && ./start-tor-browser.desktop" > \
+/usr/bin/start-tor-browser
